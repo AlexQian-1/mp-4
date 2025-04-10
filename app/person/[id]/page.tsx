67cfb6ca03fde91ffea85034eeb21e7f04c6a934
ZihaoQian <zihaoq@bu.edu>
2025-04-10 01:22:43 -0400

    mp-4 fixed

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,15 +1,13 @@
 
-import {GetPersonById} from "@/app/lib/museumApi";
-type Props = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }
-export default async function PersonDetail({params}: Props) {
-    
+import {GetPersonById} from "@/app/api/getPersonData/museumApi";
 
-    const getParams = await params;
-    const data = await GetPersonById(Number(getParams.id));
-    //https://github.com/vercel/next.js/discussions/46131
+export default  async  function PersonDetail({params}: { params: Promise<{ id: string }>}) {
+
+    const { id } = await params;
+    // const getParams = await params;
+     const data = await GetPersonById(Number( id));
+    //https://stackoverflow.com/questions/77412027/using-next-13-5-6-app-router-how-to-get-params-of-dynamic-route
+    //https://stackoverflow.com/questions/79124951/type-error-in-next-js-route-type-params-id-string-does-not-satis
     return (
         <div>
             <h1>Person</h1>
