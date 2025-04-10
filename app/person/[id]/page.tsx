8e248af222493c7175fa4f6670198d886d22a28d
ZihaoQian <zihaoq@bu.edu>
2025-04-10 00:41:38 -0400

    mp-4

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,7 +1,11 @@
+
 import {GetPersonById} from "@/app/api/getPersonData/museumApi";
 
-export default async function PersonDetail({params}: {params: {id: string}}) {
-    const data = await GetPersonById(Number(params.id));
+export default async function PersonDetail({params}: { params: { id: string } }) {
+    
+
+    const getParams = await params;
+    const data = await GetPersonById(Number(getParams.id));
     //https://stackoverflow.com/questions/77412027/using-next-13-5-6-app-router-how-to-get-params-of-dynamic-route
     return (
         <div>
